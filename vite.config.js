cef641370b04766b2295a1126170a3d5ab4b4bae
prapavarine <prapavarine.c@buzzebees.com>
2024-05-18 14:42:58 +0700

    edit script deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/final-project/', // เพิ่ม base URL สำหรับ GitHub Pages
   plugins: [react()],
 })
